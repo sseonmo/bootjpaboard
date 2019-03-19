45e0e6a3a672d1a2ab63c09701a804f4aaa4e84e
seonnmo <mo3509@gmail.com>
2019-03-20 07:00:09 +0900

    [2019.03.20] first commit

---
+++ src/main/java/me/seon/bootjpaboard/BootjpaboardApplication.java
@@ -0,0 +1,13 @@
+package me.seon.bootjpaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootjpaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootjpaboardApplication.class, args);
+	}
+
+}
