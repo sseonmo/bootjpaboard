e5049e938affa2b21587a8b065d436c1e3d0b3b6
seonnmo <mo3509@gmail.com>
2019-05-02 07:16:42 +0900

    [2019.05.02] step6 : 6-6 : swagger 적용 - 기본만 적용 허술함.

--- src/main/java/me/seon/bootjpaboard/BootjpaboardApplication.java
+++ src/main/java/me/seon/bootjpaboard/BootjpaboardApplication.java
@@ -2,14 +2,51 @@ package me.seon.bootjpaboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.ant;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+// domain에 생성일, 수정일을 자동 매핑해 준다.
+// domain class에 어노테이션 설정필요함. - @EntityListeners(AuditingEntityListener.class)
 @EnableJpaAuditing
+@EnableSwagger2
 public class BootjpaboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootjpaboardApplication.class, args);
 	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("greetings")
+				.apiInfo(apiInfo())
+				.select()
+//				.paths(regex("/api.*"))
+				.paths(ant("/api/**"))
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Spring REST Sample with Swagger")
+				.description("Spring REST Sample with Swagger")
+				.termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+				.contact("Niklas Heidloff")
+				.license("Apache License Version 2.0")
+				.licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+				.version("2.0")
+				.build();
+	}
+
+
 
 }
--- src/main/java/me/seon/bootjpaboard/domain/AbstractEntity.java
+++ src/main/java/me/seon/bootjpaboard/domain/AbstractEntity.java
@@ -2,15 +2,11 @@ package me.seon.bootjpaboard.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -19,7 +15,7 @@ import java.util.Objects;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-@Setter @Getter
+@Getter
 class AbstractEntity {
 
 	@Id
